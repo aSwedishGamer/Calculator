8f85f386ec2ffdba68fffe74477e9a951efea576
aSwedishGamer <david.carbeskar@gmail.com>
2023-12-08 17:38:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//the elements
+let addition = document.getElementById("addition");
+let subtration = document.getElementById("subtration");
+let multiplication = document.getElementById("multiplication");
+let division = document.getElementById("division");
+
+//Buttons
+addition.addEventListener("onclick")
